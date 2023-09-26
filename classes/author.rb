734a2e3dfc8cb30ef3f8f7f00eb5d292ba9d33e9
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2023-09-26 20:36:13 +0430

    Methods for game and author

--- classes/author.rb
+++ classes/author.rb
@@ -1,12 +1,18 @@
 require 'securerandom'
 
 class Author
-  attr_accessor :first_name, :last_name
-  attr_reader :id, :items
+  attr_accessor :first_name, :last_name, :items
+  attr_reader :id
 
   def initialize(first_name, last_name)
     @id = SecureRandom.uuid
     @first_name = first_name
     @last_name = last_name
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+    item.author = self
   end
 end
